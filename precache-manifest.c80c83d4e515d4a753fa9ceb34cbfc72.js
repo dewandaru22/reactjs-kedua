13de899ec871ff13f2de895dffcb2b7bb98f4743
dewandaru22 <stevenaji94@gmail.com>
2019-03-28 08:25:58 +0700

    Updates

---
+++ precache-manifest.c80c83d4e515d4a753fa9ceb34cbfc72.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/reactjs-kedua/static/media/logo.ee7cd8ed.svg"
+  },
+  {
+    "revision": "8638f61d78e838b54b33",
+    "url": "/reactjs-kedua/static/js/runtime~main.950e00de.js"
+  },
+  {
+    "revision": "622c382ccc9da6126568",
+    "url": "/reactjs-kedua/static/js/main.7ef5f4f3.chunk.js"
+  },
+  {
+    "revision": "3c688a83c6a96b1bd515",
+    "url": "/reactjs-kedua/static/js/2.f885ce20.chunk.js"
+  },
+  {
+    "revision": "622c382ccc9da6126568",
+    "url": "/reactjs-kedua/static/css/main.f024a92d.chunk.css"
+  },
+  {
+    "revision": "066adf38b44623b97fdaffe4b16eb349",
+    "url": "/reactjs-kedua/index.html"
+  }
+];
